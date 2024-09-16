24eea45c789cf818042b6c190a4264492fae18e2
KateThePrincess <k99rzonca@gmail.com>
2024-09-17 00:00:19 +0200

    clerk middleware

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,11 @@
 'use client';
 import { ReactNode } from 'react';
 import ThemeProvider from './theme-provider';
-
+import { Toaster } from '@/components/ui/toaster';
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute='class'
         defaultTheme='light'
