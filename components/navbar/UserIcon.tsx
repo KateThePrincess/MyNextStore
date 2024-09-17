b486295c9150da393e91cf2639c195a2fc9fc814
KateThePrincess <k99rzonca@gmail.com>
2024-09-17 13:34:12 +0200

    change profile image to next/image

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,17 +1,22 @@
 import { auth, currentUser } from '@clerk/nextjs/server';
+import Image from 'next/image';
 import { RxPerson } from 'react-icons/rx';
 export default async function UserIcon() {
   const { userId } = auth();
   const user = await currentUser();
   const profileImage = user?.imageUrl;
-
   if (profileImage) {
     return (
-      <img
-        src={profileImage}
-        alt={user.firstName?.toString()}
-        className='w-6 h-6 rounded-full object-cover'
-      />
+      <div className='relative w-6 h-6'>
+        <Image
+          src={profileImage}
+          alt={user.firstName?.toString() || 'User profile'}
+          className='w-6 h-6 rounded-full object-cover'
+          fill
+          priority
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+        />
+      </div>
     );
   }
   return <RxPerson className='w-6 h-6 rounded-full bg-primary text-white ' />;
